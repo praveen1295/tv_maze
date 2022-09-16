90842fe9640127be65a196e8fac0eb1b4a89c0ca
Praveen Bankhede <bankhedepraveen12@gmail.com>
2022-09-16 13:01:47 +0530

    navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,18 @@ const Navbar = () => {
             <span style={{ color: "orange" }}>TV_MAZE</span>
           </Link>
 
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
